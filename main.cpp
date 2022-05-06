34e5ba869427c0388009daaaf8a38b7b9698ff7f
Andrew Brettin <andrewbrettin@gmail.com>
2022-05-06 19:54:55 -0400

    my stuff

--- main.cpp
+++ main.cpp
@@ -21,68 +21,57 @@ int main(int argc, char *argv[])
     // Set random seed:
     srand48(0);
 
-   // Initialize parameters
-   long n     = 4000;        // Number of rows in A
-   long d     = 4000;         // Number of cols in A
-   double eta = 0.01;       // Learning rate
-   long T     = 100;       // Number of iterations of stochastic gradient descent
+    // Initialize parameters
+    long n     = 4000;         // Number of rows in A
+    long d     = 3000;          // Number of cols in A
+    double eta = 0.0001 / (n*d);       // Learning rate
+    long T     = 100;           // Number of iterations of stochastic gradient descent
 
-   // Initialize matrices
-   double* A       = (double*) malloc(n * d * sizeof(double));    // (n x d) data matrix
-   double* b       = (double*) malloc(n * sizeof(double));        // (n x 1) vector
-   double* x       = (double*) malloc(d * sizeof(double));        // (d x 1) vector
-   double* x_store = (double*) malloc(d * sizeof(double));        // (d x 1) vector
-   double* At      = (double*) malloc(d * n * sizeof(double));    // (d x n) data matrix (transpose of A)
-   double* r       = (double*) malloc(n * sizeof(double));        // (n x 1) vector (r = Ax-b)
-   // vector<long> I(n);       // contains numbers from 0 to n-1, used for reshuffling
-   
-   // set up data points
-//   for (long i=0; i<n*d; i++)
-//       A[i] = drand48();
-   for (long i=0; i<n; i++)
-   {
-       for (long j=0; j<d; j++)
-       {
-	   if (i == j)
-	       A[i+n*j] = 1.0;
-	   else
-	       A[i+n*j] = 0.0;
-       }
-   }	   
-
-   for (long i=0; i<n; i++) {
-       b[i] = drand48();
-       // I[i] = i;
-   }
-   for (long i=0; i<d; i++) {
-       x[i] = drand48();
-       x_store[i] = x[i];
-   }
-   transpose(d, n, A, At); // create transposed matrix for gradient descent
-
-
-   // gradientDescent(n, d, A, At, x, b, r, eta, T); //run gradient descent
-   // printf("\n");
-
-   for (long i=0; i<d; i++) //reset x
-       x[i] = x_store[i];
-
-   // eta = eta*20;
-   
-   // Run stochastic gradient descent
-   SGD(n, d, T, eta, A, x, b, r, RG, num_of_threads);
-   
-   for (long i=0; i<d; i++) //reset x
-       x[i] = x_store[i];
-
-   // SGD_min(n, d, T, eta, A, x, b, r, RG, num_of_threads); //run stochastic gradient descent
+    // Initialize matrices
+    double* A       = (double*) malloc(n * d * sizeof(double));    // (n x d) data matrix
+    double* b       = (double*) malloc(n * sizeof(double));        // (n x 1) vector
+    double* x       = (double*) malloc(d * sizeof(double));        // (d x 1) vector
+    double* x_store = (double*) malloc(d * sizeof(double));        // (d x 1) vector
+    double* At      = (double*) malloc(d * n * sizeof(double));    // (d x n) data matrix (transpose of A)
+    double* r       = (double*) malloc(n * sizeof(double));        // (n x 1) vector (r = Ax-b)
+    // vector<long> I(n);       // contains numbers from 0 to n-1, used for reshuffling
     
-   // Free memory
-   free(A);
-   free(b);
-   free(x);
-   free(At);
-   free(r);   
+    // set up data points
+    for (long i=0; i<n*d; i++)
+        A[i] = drand48();
+
+    for (long i=0; i<d; i++) {
+        x[i] = drand48();
+        x_store[i] = x[i];
+    }
+
+    transpose(d, n, A, At); // create transposed matrix for gradient descent
+
+    // Set b = Ax
+    MMult0(n, 1, d, A, x, b);
+
+    // gradientDescent(n, d, A, At, x, b, r, eta, T); //run gradient descent
+    // printf("\n");
+
+    for (long i=0; i<d; i++) //reset x
+        x[i] = x_store[i];
+
+    // eta = eta*20;
     
-   return 0;
+    // Run stochastic gradient descent
+    SGD(n, d, T, eta, A, x, b, r, RG, num_of_threads);
+    
+    for (long i=0; i<d; i++) //reset x
+        x[i] = x_store[i];
+
+    // SGD_min(n, d, T, eta, A, x, b, r, RG, num_of_threads); //run stochastic gradient descent
+     
+    // Free memory
+    free(A);
+    free(b);
+    free(x);
+    free(At);
+    free(r);   
+     
+    return 0;
 }
