97f34c545990f50b7981bbfa1174229ffcacf539
Brando Magnani <im975@nyu.edu>
2022-05-07 14:03:14 -0400

    tools.h, gradient_descent.h modified, eliminated MMult.h

--- final/gradient_descent.h
+++ final/gradient_descent.h
@@ -6,90 +6,66 @@
 #include <math.h>
 #include <omp.h>
 #include <stdlib.h>
-
-#include "MMult.h"
-// #include "debug.h"
+#include "tools.h"
 
 using namespace std;
 
-void transpose(long n, long d, double* A, double* At)
-{
-    // Takes the transpose of an ncd matrix A. Stores the result in At.
-    for (long i=0; i<n; i++) {
-        for (long j=0; j<d; j++) {
-            // At[j, i] = A[i, j]
-            At[i+n*j] = A[i*d+j];
-        }
-    }
+
+void gradientIteration(long n,
+                       long d,
+                       double* A,
+                       double* At,
+                       double* x,
+                       double* b,
+                       double* r,
+                       double* grad,
+                       double eta,
+                       double* Ax) {
+   
+   // Performs a single step of gradient descent
+   // Gradient is given by A^T(Ax - b)
+
+   // Update residual r = Ax - b
+   residual(n, d, A, x, b, r, Ax);
+
+   // Update grad = A^T(Ax-b) (multiply by 1/n later)
+   Matvec0(d, n, At, r, grad);
+   // MMult0(d, 1, n, At, r, grad);
+
+   // Perform iteration
+   for (long i=0; i<d; i++) {
+      x[i] = x[i] - eta * grad[i];
+   }
 }
 
-void residual(long n, long d, double* A, double* x, double* b, double* r, double* Ax)
-{
-    // Computes r = Ax - b.
-    Matvec0(d, n, A, x, Ax);
-    // MMult0(n, 1, d, A, x, Ax);
 
-    for (long i=0; i<n; i++) {
-        r[i] = Ax[i] - b[i]; //1 flop -> n flops
-    }
+void gradientDescent(long n,
+                     long d,
+                     double* A,
+                     double* At,
+                     double* x,
+                     double* b,
+                     double* r,
+                     double eta,
+                     long n_iter,
+                     double sf) {
 
-}
-
-double norm(double* r, long n)
-{
-    // Computes the 2-norm of r
-    float mag = 0.0;
-    for (long i=0; i<n; i++) {
-        mag = mag + r[i]*r[i];
-    }
-    return sqrt(mag);
-}
-
-void gradientIteration(long n, long d, double* A, double* At, 
-                       double* x, double* b, double* r, double* grad,
-                       double eta, double* Ax)
-{
-    // Performs a single step of gradient descent
-    // Gradient is given by A^T(Ax - b)
-
-    // Update residual r = Ax - b
-    residual(n, d, A, x, b, r, Ax);
-
-    // Reset gradient
-    // for (long i=0; i<d; i++)
-    //     grad[i] = 0.0;
-    // Update grad = A^T(Ax-b) (multiply by 1/n later)
-     Matvec0(n, d, At, r, grad); 
-    // MMult0(d, 1, n, At, r, grad); 
-
-    // Perform iteration
-    for (long i=0; i<d; i++) {
-        x[i] = x[i] - eta * grad[i];
-    }
-}
-
-void gradientDescent(long n, long d, double* A, double* At, 
-                     double* x, double* b, double* r,
-                     double eta, long n_iter, double sf)
-{
-    //printf("Iteration | Residual\n");
-
-    double* grad   = (double*) malloc(d * sizeof(double));        // (d x 1) vector for grad(F_i(x))
-    double* Ax = (double*) malloc(n * sizeof(double));
-    double tt = omp_get_wtime();
-    residual(n, d, A, x, b, r, Ax);
-    double tol = sf * norm(r, n);
-    for (long i=0; i<n_iter; i++) 
-    {
-        gradientIteration(n, d, A, At, x, b, r, grad, eta, Ax);
-        double res = norm(r, n);
-        printf("%f, %f\n", res, omp_get_wtime()-tt);
-        if (res < tol) {
-            break;
-        }
-    }
-    free(grad);
-    free(Ax);
+   double* grad   = (double*) malloc(d * sizeof(double));        // (d x 1) vector for grad(F_i(x))
+   double* Ax = (double*) malloc(n * sizeof(double));
+   double tt = omp_get_wtime();
+   residual(n, d, A, x, b, r, Ax);
+   double tol = sf * norm(r, n);
+   
+   for (long i=0; i<n_iter; i++){
+      gradientIteration(n, d, A, At, x, b, r, grad, eta, Ax);
+      double res = norm(r, n);
+      printf("%f, %f\n", res, omp_get_wtime()-tt);
+      if (res < tol) {
+         break;
+      }
+   }
+   free(grad);
+   free(Ax);
 }
 
 #endif
